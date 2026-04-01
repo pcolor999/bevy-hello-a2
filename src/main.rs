8c0787a41a92af21aea926d57debf3e3a10bd2b6
pcolor999 <109137873+pcolor999@users.noreply.github.com>
2026-04-01 10:14:40 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    // Add a 2D Camera (Notice we just use `Camera2d` now instead of the bundle)
+    commands.spawn(Camera2d);
+
+    // Add a red square (Notice we just use `Sprite` now)
+    commands.spawn(Sprite {
+        color: Color::srgb(1.0, 0.0, 0.0),
+        custom_size: Some(Vec2::new(100.0, 100.0)),
+        ..default()
+    });
 }
